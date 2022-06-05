9eddba489676d4eba6f4a76cb21c1e44cbec1a15
Marlon Mavinier <mavinier2010@gmail.com>
2022-06-05 14:35:11 -0300

    configuring db

--- fs-challenge/backend/src/server-config/routes.ts
+++ fs-challenge/backend/src/server-config/routes.ts
@@ -1,11 +1,18 @@
 import Router from "@koa/router";
+import { PrismaClient } from "@prisma/client";
 
 export const router = new Router();
+const prisma = new PrismaClient();
 
-router.get("/tweets", (ctx) => {
-  ctx.body = [];
+router.get("/tweets", async (ctx) => {
+  const tweets = await prisma.tweet.findMany();
+  ctx.body = tweets;
 });
 
-router.post("/tweets", (ctx) => {
-  ctx.body = {};
+router.post("/tweets", async (ctx) => {
+  const doc = await prisma.tweet.create({
+    data: ctx.request.body,
+  });
+
+  ctx.body = doc;
 });
