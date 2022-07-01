98795bb7f229982d681c698072af7df5c0cd20b1
Marlon Mavinier <mavinier2010@gmail.com>
2022-07-01 10:17:21 -0300

    feat: docker-compose for api and frontend

--- fs-challenge/frontend/vite.config.ts
+++ fs-challenge/frontend/vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 import EnvironmentPlugin from 'vite-plugin-environment';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
   plugins: [react(), EnvironmentPlugin(['VITE_API_HOST'])],
 });
